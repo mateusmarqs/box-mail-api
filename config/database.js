d36f08dfffa4b0e2e6eab01697756e5a5c6fbef9
Mateus Marques <mateusmarquesdc@gmail.com>
2021-08-17 23:52:00 -0300

    Versão inicial da página /index e /login. Pequenas alterações na estrutura do projeto.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/boxmail', { useNewUrlParser: true, useUnifiedTopology: true })
 
-mongoose.connect('mongodb://localhost:27017/boxmail', { useMongoClient: true })
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Conectado ao MongoBD')
+});
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const silence = new Kitten({ name: 'Silence' });
--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var app = require('./config/express')()
-var http = require('http').createServer(app) // Express vai estar rodando no servidor HTTP nativo do node
-var io = require('socket.io')(http)
+const express = require('express')
+const app = express()
+const port = 3000
+// var http = require('http').createServer(app) // Express vai estar rodando no servidor HTTP nativo do node
+// var io = require('socket.io')(http)
 
-require('./config/database')
+// require('./config/database')
 
-io.on('connection', socket => {
-    socket.on('msg', data => {
-        console.log(data)
-        io.emit('showMsg', data)
-    })
-})
+// Express config
+app.use(express.static('./views'));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs')
+app.use(express.urlencoded ({
+    extended: false
+}))  
+app.use(express.json())
+
+// io.on('connection', socket => {
+//     socket.on('msg', data => {
+//         console.log(data)
+//         io.emit('showMsg', data)
+//     })
+// })
 
 app.get('/', (req, res) => {
     res.render('index')    
 })
 
-http.listen(3000, () => {
-    console.log('Sevidor rodando...')
+app.get('/login', (req, res) => {
+    res.render('login')    
+})
+
+app.listen(port, () => {
+    console.log(`Sevidor rodando na porta ${port}`)
 })
